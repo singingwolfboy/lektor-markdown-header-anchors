6f1500a7104a5f03f353df6236dfca48158c7ae9
David Baumgold <david@davidbaumgold.com>
2016-05-20 07:33:53 -0700

    Add feature to adjust header levels

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 This plugin extends the markdown support in Lektor in a way that headlines
 are given anchors and a table of contents is collected.
 
+It also allows you to adjust the heading level in the HTML output: converting
+all `<h1>` tags to `<h2>` tags, and so on.
+This feature should be a separate plugin, but due to the way Lektor plugins
+are structured, two plugins that both modify Markdown headers
+will clobber each other.
+
 ## Enabling the Plugin
 
 To enable the plugin run this command:
@@ -31,3 +37,23 @@ Example rendering:
 {% endfor %}
 </ul>
 ```
+
+## Adjusting Header Levels
+
+To modify the header level of a markdown field, set the
+`markdown_header_adjustment` option on that field in the model. For example:
+
+```ini
+[fields.body]
+label = Body
+type = markdown
+markdown_header_adjustment = 1
+```
+
+This option must be an integer. The value of the integer is added to the level
+of the generated header tags in the HTML. With a value of 1, all `<h1>` tags
+become `<h2>` tags, and all `<h2>` tags become `<h3>` tags.
+With a value of 2, all `<h1>` tags become `<h3>` tags, and all `<h3>` tags
+become `<h4>` tags. HTML only defines header tags from `<h1>` to `<h6>`,
+so this plugin will bound the output between those two values:
+it will never generate an `<h7>` tag, for example.
--- lektor_markdown_header_anchors.py
+++ lektor_markdown_header_anchors.py
@@ -1,4 +1,5 @@
 from lektor.pluginsystem import Plugin
+from lektor.types.formats import MarkdownType
 from lektor.utils import slugify
 from markupsafe import Markup
 from collections import namedtuple
@@ -13,10 +14,46 @@ class MarkdownHeaderAnchorsPlugin(Plugin):
 
     def on_markdown_config(self, config, **extra):
         class HeaderAnchorMixin(object):
+            def get_header_adjustment(renderer):
+                """
+                If the model has a `markdown_header_adjustment` option defined
+                on a markdown field, return that option value as an integer.
+                If not, return 0.
+                """
+                markdown_fields = [
+                    field for field in renderer.record.datamodel.fields
+                    if isinstance(field.type, MarkdownType)
+                ]
+                adjustments = [
+                    field.options.get("markdown_header_adjustment")
+                    for field in markdown_fields
+                    if field.options.get("markdown_header_adjustment")
+                ]
+                if adjustments:
+                    # need a way to figure out which field is being rendered
+                    # but for now, arbitrarily take the first one
+                    adjustment = adjustments[0]
+                    return int(adjustment)
+                else:
+                    return 0
+
+            def adjusted_header_level(renderer, level):
+                """
+                Return the adjusted header level as modified by the
+                `markdown_header_adjustment` option, bounded between 1 and 6.
+                """
+                adjustment = renderer.get_header_adjustment()
+                return max(1, min(6, level + adjustment))
+
             def header(renderer, text, level, raw):
                 anchor = slugify(raw)
                 renderer.meta['toc'].append((level, anchor, Markup(text)))
-                return '<h%d id="%s">%s</h%d>' % (level, anchor, text, level)
+                return '<h{level:d} id="{anchor}">{text}</h{level:d}>'.format(
+                    level=renderer.adjusted_header_level(level),
+                    anchor=anchor,
+                    text=text,
+                )
+
         config.renderer_mixins.append(HeaderAnchorMixin)
 
     def on_markdown_meta_init(self, meta, **extra):
